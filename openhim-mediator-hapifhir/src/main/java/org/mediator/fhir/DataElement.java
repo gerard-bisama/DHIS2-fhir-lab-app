82ffd584bc981ad149135bda40e0bbdc17244eab
gerard-bisama <gerbis2000@gmail.com>
2017-05-03 14:56:15 +0100

    Associated class to manage dynamic dataelement mapping

---
+++ openhim-mediator-hapifhir/src/main/java/org/mediator/fhir/DataElement.java
@@ -0,0 +1,9 @@
+package org.mediator.fhir;
+
+/**
+ * Created by server-hit on 4/26/17.
+ */
+public class DataElement {
+    public String displayName;
+    public String id;
+}
---
+++ openhim-mediator-hapifhir/src/main/java/org/mediator/fhir/PatientFhirMapping.java
@@ -0,0 +1,18 @@
+package org.mediator.fhir;
+
+/**
+ * Created by server-hit on 5/2/17.
+ */
+public class PatientFhirMapping {
+    public String id;
+    public String managingOrganization;
+    public String identifier;
+    public String name_family;
+    public String name_given;
+    public String telecom_phone;
+    public String telecom_email;
+    public String gender;
+    public String birthDate;
+    public String address;
+    public String deceasedBoolean;
+}
